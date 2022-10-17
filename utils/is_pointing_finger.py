643f2d474e98ba3ad5304d205b0239e9710a3bd2
Diego Medeiros <diego.medeiros@oceanbrasil.com>
2022-10-17 11:47:58 -0400

    Lógica do dedo apontado

--- utils/is_pointing_finger.py
+++ utils/is_pointing_finger.py
@@ -11,13 +11,23 @@ def is_pointing_gesture(hand_landmarks):
     index_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_TIP].y
     index_finger_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.INDEX_FINGER_MCP].y
 
-    middle_finger_tip = None
-    middle_finger_mcp = None
+    middle_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_TIP].y
+    middle_finger_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y
 
-    ring_finger_tip = None
-    ring_finger_mcp = None
+    ring_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_TIP].y
+    ring_finger_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.RING_FINGER_MCP].y
 
-    pinky_finger_tip = None
-    pinky_finger_mcp = None
+    pinky_finger_tip = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_TIP].y
+    pinky_finger_mcp = hand_landmarks.landmark[mp_hands.HandLandmark.PINKY_MCP].y
+
+    # Y1 > Y2 -> abaixado
+    # Y1 < Y2 -> levantado
+    if(
+        index_finger_tip < index_finger_mcp and
+        middle_finger_tip > middle_finger_mcp and
+        ring_finger_tip > ring_finger_mcp and
+        pinky_finger_tip > pinky_finger_mcp
+    ):
+        return True
 
     return False
